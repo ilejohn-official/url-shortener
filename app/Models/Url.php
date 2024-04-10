89e86e5ff6f5de4f798df92907d3507dc6b7b61a
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2024-04-10 10:46:40 +0100

    modify table column and use accessors

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,5 +10,22 @@ class Url extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['url', 'short_url', 'short_url_hash'];
+    protected $fillable = ['url', 'short_url_hash'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['short_url'];
+
+    /**
+     * Dynamically retrieve the short_url.
+     */
+    protected function shortUrl(): Attribute
+    {
+        return new Attribute(
+            get: fn () => config('app.url') . '/' . $this->short_url_hash,
+        );
+    }
 }
--- database/migrations/2024_04_08_221410_create_urls_table.php
+++ database/migrations/2024_04_08_221410_create_urls_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-            $table->string('short_url')->unique();
             $table->string('short_url_hash')->unique();
             $table->timestamps();
         });
